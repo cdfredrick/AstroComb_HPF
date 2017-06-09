7c6a0405f68e961f82366006c74ea66936c2010c
AJ Metcalf <andrew.metcalf@colorado.edu>
2017-06-08 18:01:41 -0600

    Update Finisar.py

--- DRIVERS/Finisar/Finisar.py
+++ DRIVERS/Finisar/Finisar.py
@@ -10,12 +10,10 @@ Initiate:
     __init__(serial_number)
     
         Example Call: 
-            resource = Finisar(serial number)
-    
+            resource = Finisar(serial number)  
 General:
     open_communication(resource)
     close_communication(resource)
-
 Write:
     write_mask(resource,mask)
     
@@ -24,20 +22,20 @@ Write:
             amp = np.ones((dim,1))
             phase = np.zeros((dim,1))
             port = np.ones((dim,1))
-            buffer = "%7.3f" % WS.StartF + "\t" + "%6.3f" % amp[0] + "\t" + 
-            "%8.6f" % phase[0] + "\t" + "%1i" % phase[0] + "\n"
 
 for i in range(1,int(dim)):
     buffer = buffer + "%7.3f" % (WS.StartF+0.001*i-0.001) + "\t" + 
     "%6.3f" % amp[i] + "\t" + "%8.6f" % phase[i] + "\t" + "%1i" % phase[i] + "\n"
 """
-
 import wsapi as ws
 import math
 import numpy as np
 
 class Finisar:
     def __init__(self,SN):
+        """Clears values for start and stop frequency, and creates a waveshaper instance.  
+        Note: You still cannot write to device without first opening communicaiton using 
+        the open_communication function"""
         self.StartF = 0     #Start Frequency in THz
         self.StopF = 0      #Stop Frequency in THz
         self.SN = SN
@@ -46,6 +44,7 @@ class Finisar:
         print "ws_create_waveshaper rc="+str(ws.ws_get_result_description(self.rc))
 
     def open_communication(self):
+        """Opens Communication with waveshaper, allowing you to write and read from it"""
         self.rc = ws.ws_open_waveshaper("ws1")
         print "ws_open_waveshaper rc=" + str(ws.ws_get_result_description(self.rc))
         #rc = ws.ws_open_waveshaper("ws1")
@@ -54,7 +53,10 @@ class Finisar:
         self.StopF = ws.ws_get_stopfreq("ws1")
         print "Stop Frequency = " + str(self.StopF) + " THz"
         
-    def write_mask(self,amp,phase,port,dim):    
+    def write_mask(self,amp,phase,port,dim):  
+        """Writes amplitude, phase, and port vectors to waveshaper
+        vectors should all be V(1,dim) in dimension.
+        format, freq(xxx.xxx) TAB amp(xx.xxx) TAB phase(x.xxxxxx) TAB port(x) NewLine"""
         buffer = "%7.3f" % self.StartF + "\t" + "%6.3f" % amp[0] + "\t" + "%8.6f" % phase[0] + "\t" + "%1i" % phase[0] + "\n"                                                 
         for i in range(1,int(dim)):
             buffer = buffer + "%7.3f" % (self.StartF+0.001*i-0.001) + "\t" + "%6.3f" % amp[i] + "\t" + "%8.6f" % phase[i] + "\t" + "%1i" % phase[i] + "\n"
@@ -62,6 +64,7 @@ class Finisar:
         print "ws_write_waveshaper rc=" + str(ws.ws_get_result_description(self.rc))
         
     def close_communication(self):
+        """Closes communication with waveshaper and deletes the waveshaper instance"""
         self.rc = ws.ws_close_waveshaper("ws1")
         print "ws_close_waveshaper rc=" + str(ws.ws_get_result_description(self.rc))
         self.rc = ws.ws_delete_waveshaper("ws1")

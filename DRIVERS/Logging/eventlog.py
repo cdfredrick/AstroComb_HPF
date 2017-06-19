7ca3b75fea4b4bb3cd1b13e63a91ecd706a95c04
wesley-brand <wesley.brand@colorado.edu>
2017-06-19 15:24:14 -0600

    It works now

--- DRIVERS/Logging/eventlog.py
+++ DRIVERS/Logging/eventlog.py
@@ -2,21 +2,30 @@
 """
 Created on Thu Jun 15 12:56:31 2017
 
-Public functions:
+Module: eventlog
+    import eventlog as log
 
-start_logging()
-log_this(log_string, level)
+Requires:
+    logging.conf
+    #To decrease stuff in log file increase the level under astroComb to INFO or higher
+
+Public functions:
+    start_logging()
+    log_this(level1=10, level2=10, log_string1='', log_string2='')
+    log_error(func, err, log_str='', level=40)
 
 
 @author: Wesley Brand
 """
+#pylint: disable=W0702
+
 import logging
 import logging.config
 from functools import wraps
 
 def start_logging():
     """Must be called by external module to begin logging
-    
+
     logs to astroComb.log
     see logging.conf for format details"""
     logging.config.fileConfig('logging.conf')
@@ -24,9 +33,9 @@ def start_logging():
     logger.info('Logging started!')
     return logger
 
-def log_this(log_string, level):
-    """Takes string that says what is being logged and an integer for the level
-    
+def log_this(level1=10, level2=10, log_string1='', log_string2=''):
+    """Takes optional integers for logging levels and optional descriptive strings
+
     Levels |   10  |  20  |   30    |   40  |    50    |
            | debug | info | warning | error | critical |"""
     def function_decorator(func):
@@ -35,8 +44,27 @@ def log_this(log_string, level):
         def wrapper(*args, **kwargs):
             """The new function wtih logging"""
             logger = logging.getLogger('astroComb.%s.%s' % (func.__module__, func.__name__))
-            message = log_string + ' with arguments: ' + str(args) + str(kwargs)
-            logger.log(level, message)    
-            func(*args, **kwargs)
+            message = log_string1 + ' Arguments: ' + str(args) + str(kwargs)
+            logger.log(level1, message)
+            result = func(*args, **kwargs)
+            try:
+                res_str = str(result)
+                print res_str
+            except:  #Many possible reasons a result is not string-able
+                res_str = '{}'
+            message = log_string2 + ' Returns: ' + res_str
+            logger.log(level2, message)
+            return result
         return wrapper
     return function_decorator
+
+def log_error(module, name, err, log_str='', level=40):
+    """Takes originating function name and module and error to log, optional string & log level
+
+    Note that if function is in error handling function decorator you'll need
+    to use functools.wraps on the function in th error handler that calls log_error
+    Levels |   10  |  20  |   30    |   40  |    50    |
+           | debug | info | warning | error | critical |"""
+
+    logger = logging.getLogger('astroComb.%s.%s' % (module, name))
+    logger.log(level, str(err) + log_str)

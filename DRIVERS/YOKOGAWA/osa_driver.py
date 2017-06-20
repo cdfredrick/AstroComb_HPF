8d6f425a1b075ee7a4d6e387cc6cb853fe5ca5b3
wesley-brand <wesley.brand@colorado.edu>
2017-06-20 12:41:22 -0600

    Add files via upload

--- DRIVERS/YOKOGAWA/osa_driver.py
+++ DRIVERS/YOKOGAWA/osa_driver.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jun 20 07:26:32 2017
-
 @author: ajm6
 """
 
@@ -16,7 +15,7 @@ OSA_NAME = 'OSA'
 OSA_ADDRESS = u'GPIB0::28::INSTR'
 
 class OSA(vo.Visa):
-    #@log.log_this()
+    @log.log_this(20)
     def __init__(self, res_name, res_address):
         super(OSA, self).__init__(res_name, res_address)
         self.res = super(OSA, self).open_resource()
@@ -24,19 +23,19 @@ class OSA(vo.Visa):
             print 'Could not create OSA instrument!'
             return
         
-    #@log.log_this()  
+    @log.log_this()  
     def close(self):
         """Ends device session"""
         self.res.close()
         
     @vo.handle_timeout
-    #@log.log_this()
+    @log.log_this()
     def test(self):
         """Returns 0 if all good, not 0 if a test fails"""
         return self.res.query('*TST?')
         
     @vo.handle_timeout
-    #@log.log_this()
+    @log.log_this()
     def read(self):
         """Queries Laser Diode Unit's current operating values"""
         osa_data = self.res.query('*IDN?')
@@ -44,7 +43,7 @@ class OSA(vo.Visa):
         
         
     @vo.handle_timeout
-    #@log.log_this()
+    @log.log_this()
     def write(self):
         """Queries Laser Diode Unit's current operating values"""
         temp = self.res.write('CFORM1')
@@ -67,5 +66,3 @@ class OSA(vo.Visa):
         plt.ylabel('dBm')
         plt.grid(True) 
         plt.show()
-
-        
---
+++ DRIVERS/YOKOGAWA/test_osa.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 14:19:40 2017
+@author: ajm6
+"""
+
+import osa_driver as yok
+import eventlog as log
+
+log.start_logging()
+YOKO = yok.OSA(yok.OSA_NAME, yok.OSA_ADDRESS)
+if YOKO.res is None:
+    raise SystemExit
+YOKO.read()
+YOKO.write()
+YOKO.close()
+
+
+
+#"""Look for directory with Yokogawa which contains OSA spectrum Files, 
+#create directory if it does not exist"""
+#directory = "YokogawaFiles"
+#if not os.path.exists(directory):
+#    os.makedirs(directory)   
+#testString = directory + "/Yokogawa_"
+#extension = ".txt"
+#currentCount = 0
+
+#"""Look for highest numberd file in /YokogawaFiles/ sub directory and return
+#file name of latest file number + 1 then create file"""
+#def checkFilePath(testString, extension, currentCount):
+#    if os.path.exists(testString + str(currentCount) +extension):
+#        return checkFilePath(testString, extension, currentCount+1)
+#    else:
+#        return testString + str(currentCount) +extension
+
+
+#Ans = checkFilePath(testString,extension,currentCount)
+#F=open(Ans,"w+")
+#print Ans
+
+#F.close()

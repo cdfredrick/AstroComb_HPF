fffb1947f83f0cfcc1049ed853af33bb595cc84e
wesley-brand <wesley.brand@colorado.edu>
2017-06-21 11:51:32 -0600

    Add files via upload

--- DRIVERS/YOKOGAWA/osa_driver.py
+++ DRIVERS/YOKOGAWA/osa_driver.py
@@ -23,12 +23,52 @@ with Public Methods:
 
 """
 
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 import visa_objects as vo
 import eventlog as log
 
 OSA_NAME = 'OSA'
 OSA_ADDRESS = u'GPIB0::28::INSTR'
+EXT = '.txt'
+
+def plot_spectrum(data):
+    """Plots spectrum from yokogawa"""
+    (lambdas, levels) = data.T
+    plt.plot(lambdas, levels)
+    plt.xlabel('wavelength nm')
+    plt.ylabel('dBm')
+    plt.grid(True)
+    plt.show()
+
+def _find_directory():
+    """Look for directory with Yokogawa which contains OSA spectrum Files,
+    create directory if it does not exist"""
+    directory = 'YokogawaFiles'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory + '/Yokogawa_'
+
+def _check_file_path(file_string, count):
+    """Look for highest numberd file in /YokogawaFiles/ sub directory and return
+    file name of latest file number + 1 then create file"""
+    while True:
+        if os.path.exists(_form_file_name(file_string, count)):
+            count += 1
+        else: break
+    return count
+
+def _form_file_name(file_string, count):
+    """Formats a file name string for saving"""
+    return file_string + str(count).zfill(4) + EXT
+
+@log.log_this()
+def _write_data_file(data, file_name):
+    """Saves OSA spectrum in tab delimited txt file"""
+    data_file = open(file_name, 'w')
+    np.savetxt(data_file, data, delimiter='\t')
+    data_file.close()
 
 class OSA(vo.Visa):
     """Holds Yokogawa OSA's attributes and method library."""
@@ -40,19 +80,20 @@ class OSA(vo.Visa):
             print 'Could not create OSA instrument!'
             return
         self.__set_command_format()
+        self.file_string = _find_directory()
+        self.file_count = _check_file_path(self.file_string, 0)
+        self.file_name = _form_file_name(self.file_string, self.file_count)
 
     @log.log_this()
     def close(self):
         """Ends device session"""
         self.res.close()
 
-
     @vo.handle_timeout
     @log.log_this()
     def __set_command_format(self):
         """Sets the OSA's formatting to AQ6370 style, should always be 1"""
         self.res.write('CFORM1')
-
 
     @vo.handle_timeout
     @log.log_this()
@@ -61,9 +102,18 @@ class OSA(vo.Visa):
         ident = self.res.query('*IDN?')
         print 'OSA Identity = %s' % ident
 
+    @log.log_this()
+    def save_n_graph_spectrum(self):
+        """Prints a graph of osa spectrum to console and saves values to a file"""
+        data = self._query_spectrum()
+        _write_data_file(data, self.file_name)
+        self.file_count += 1
+        self.file_name = _form_file_name(self.file_string, self.file_count)
+        plot_spectrum(data)
+
     @vo.handle_timeout
     @log.log_this()
-    def query_spectrum(self):
+    def _query_spectrum(self):
         """Queries OSA's spectrum"""
         y_trace = self.res.query(':TRACE:DATA:Y? TRA')
         x_trace = self.res.query(':TRACE:DATA:X? TRA')
@@ -71,8 +121,8 @@ class OSA(vo.Visa):
         levels = np.fromstring(y_trace, sep=',')
         lambdas = lambdas[1:]
         levels = levels[1:]
-        return lambdas, levels
+        data = np.array([lambdas, levels]).T
+        return data
         #startWL = float(osa.query("STAWL?")[0:-2])
         #stopWL  = float(osa.query("STPWL?")[0:-2])
         #self.lambdas = np.linspace(startWL,stopWL,nPoints)
-        
--- DRIVERS/YOKOGAWA/test_osa.py
+++ DRIVERS/YOKOGAWA/test_osa.py
@@ -1,56 +1,25 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 19 14:19:40 2017
-@author: ajm6
+@author: Wesley Brand
 """
 
-import matplotlib.pyplot as plt
+
 import osa_driver as yok
 import eventlog as log
 
 log.start_logging()
 
-def plot_spectrum(lambdas, levels):
-    """Plots spectrum from yokogawa"""
-    plt.plot(lambdas, levels)
-    plt.xlabel('wavelength nm')
-    plt.ylabel('dBm')
-    plt.grid(True)
-    plt.show()
+
 
 def test():
     """Tests some basic methods and connectability of OSA"""
     osa = yok.OSA(yok.OSA_NAME, yok.OSA_ADDRESS)
+    print osa.file_name
     if osa.res is None:
         raise SystemExit
     osa.query_identity()
-    (lambdas, levels) = osa.query_spectrum()
-    plot_spectrum(lambdas, levels)
+    osa.save_n_graph_spectrum()
     osa.close()
 
 test()
-
-
-#"""Look for directory with Yokogawa which contains OSA spectrum Files,
-#create directory if it does not exist"""
-#directory = "YokogawaFiles"
-#if not os.path.exists(directory):
-#    os.makedirs(directory)
-#testString = directory + "/Yokogawa_"
-#extension = ".txt"
-#currentCount = 0
-
-#"""Look for highest numberd file in /YokogawaFiles/ sub directory and return
-#file name of latest file number + 1 then create file"""
-#def checkFilePath(testString, extension, currentCount):
-#    if os.path.exists(testString + str(currentCount) +extension):
-#        return checkFilePath(testString, extension, currentCount+1)
-#    else:
-#        return testString + str(currentCount) +extension
-
-
-#Ans = checkFilePath(testString,extension,currentCount)
-#F=open(Ans,"w+")
-#print Ans
-
-#F.close()

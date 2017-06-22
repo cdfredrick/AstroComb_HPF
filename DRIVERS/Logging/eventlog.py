de9a532c8e9c65079e65e4d18a6ea5234783d49b
wesley-brand <wesley.brand@colorado.edu>
2017-06-22 15:53:38 -0600

    cleanup

--- DRIVERS/Logging/eventlog.py
+++ DRIVERS/Logging/eventlog.py
@@ -9,7 +9,8 @@ Module: eventlog
 
 Requires:
     logging.conf
-    #To decrease stuff in log file increase the level under astroComb to INFO or higher
+    #To decrease stuff in log file
+    increase the level under astroComb to INFO or higher
 
 Public functions:
     start_logging()
@@ -34,7 +35,7 @@ def start_logging():
     return logger
 
 def log_this(level1=10, level2=10, log_string1='', log_string2=''):
-    """Takes optional integers for logging levels and optional descriptive strings
+    """Takes optional integers for levels and optional descriptors
 
     Levels |   10  |  20  |   30    |   40  |    50    |
            | debug | info | warning | error | critical |"""
@@ -43,7 +44,8 @@ def log_this(level1=10, level2=10, log_string1='', log_string2=''):
         @wraps(func)
         def wrapper(*args, **kwargs):
             """The new function wtih logging"""
-            logger = logging.getLogger('astroComb.%s.%s' % (func.__module__, func.__name__))
+            logger = logging.getLogger('astroComb.%s.%s' % (func.__module__,
+                                                            func.__name__))
             message = log_string1 + ' Arguments: ' + str(args) + str(kwargs)
             logger.log(level1, message)
             result = func(*args, **kwargs)
@@ -59,19 +61,24 @@ def log_this(level1=10, level2=10, log_string1='', log_string2=''):
     return function_decorator
 
 def log_error(mod_name, func_name, err, log_str='', level=40):
-    """Takes originating function name and module and error to log, optional string & log level
+    """Takes originating function name and module and error to log
 
-    Note that if function is in error handling function decorator you'll need
-    to use functools.wraps on the function in th error handler that calls log_error
+    Optional descriptive string & log level
+    Note that if function is in error handling function decorator
+        you'll need to use functools.wraps on the function in the
+        error handler that calls log_error (see log_this above)
     Levels |   10  |  20  |   30    |   40  |    50    |
            | debug | info | warning | error | critical |"""
     logger = logging.getLogger('astroComb.%s.%s' % (mod_name, func_name))
     logger.log(level, str(err) + log_str)
 
 def log_warn(mod_name, func_name, log_str, level=30):
-    """Takes originating function name and module and string to log, optional level
+    """Takes originating function name and module and string to log
 
-    The most general log-what-you-want call
+    Optional level, not just for warnings
+    The most general log-what-you-want call, use instead of print
+        for final code
+    Requires more hard-coding
     Levels |   10  |  20  |   30    |   40  |    50    |
            | debug | info | warning | error | critical |"""
     logger = logging.getLogger('astroComb.%s.%s' % (mod_name, func_name))

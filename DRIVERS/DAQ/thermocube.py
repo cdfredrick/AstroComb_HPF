86b8d05e57f79ad0863a8be1190eab8882abdb04
wesley-brand <wesley.brand@colorado.edu>
2017-06-23 17:03:25 -0600

    does both channels

--- DRIVERS/DAQ/thermocube.py
+++ DRIVERS/DAQ/thermocube.py
@@ -12,70 +12,38 @@ import matplotlib.pyplot as plt
 import eventlog as log
 import daq_objects as daq
 
+def point_measure(daq_object, samples=100, rate=10000):
+    """Averages over a quick data run to return one point"""
+    daq_object.create_analog_in(samples, rate)
+    result = np.average(daq_object.read_analog_in())
+    daq_object.end_task()
+    return result
+
+def data_loop(daq1, daq2, loops):
+    """Takes some data from the DAQ"""
+    tempdata = []
+    systemdata = []
+    for i in np.arange(loops):
+        tempdata.append(point_measure(daq1))
+        systemdata.append(point_measure(daq2))
+        if tempdata[i] > 4.3:
+            print 'Temp Normal'
+        elif tempdata[i] < 0.7:
+            print 'Temp Alarm!'
+        else:
+            print 'Temp Alert'
+        if systemdata[i] > 4.3:
+            print 'System Normal'
+        elif systemdata[i] < 0.7:
+            print 'System Alarm!'
+        else:
+            print 'System Alert'
+    plt.plot(systemdata)
+    plt.show()
+    plt.plot(tempdata)
 
 log.start_logging()
 
-THERMO = daq.DAQ(0)
-
-data = []
-
-for i in np.arange(20):
-    THERMO.create_analog_in(5, 100.)
-    data.append(np.average(THERMO.read_analog_in()))
-    THERMO.end_task()
-    if data[i] > 4.3:
-        print 'Normal'
-    elif data[i] < 0.7:
-        print 'Alarm!'
-    else:
-        print 'Alert'
-plt.plot(data)
-
-
-
-
-
-
-
-
-
-
-
-
-#import numpy as np
-#import PyDAQmx
-#import matplotlib.pyplot as plt
-#
-#n_samples = 100
-#log_rate = 100.
-#
-#task_handle = daq.TaskHandle()
-#read = daq.int32()
-#data = np.zeros((n_samples,), dtype=daq.float64)
-#
-#try:
-#    # DAQmx Configure Code
-#    daq.DAQmxCreateTask('', daq.byref(task_handle))
-#    daq.DAQmxCreateAIVoltageChan(task_handle, 'Dev1/ai0', '',
-#                                 daq.DAQmx_Val_Cfg_Default, -10.0, 10.0,
-#                                 daq.DAQmx_Val_Volts, None)
-#    daq.DAQmxCfgSampClkTiming(task_handle, '', log_rate, daq.DAQmx_Val_Rising,
-#                              daq.DAQmx_Val_FiniteSamps, n_samples)
-#
-#    # DAQmx Start Code
-#    daq.DAQmxStartTask(task_handle)
-#
-#    # DAQmx Read Code
-#    daq.DAQmxReadAnalogF64(task_handle, n_samples, 10.0,
-#                           daq.DAQmx_Val_GroupByChannel, data, n_samples,
-#                           daq.byref(read), None)
-#
-#    print 'Acquired %d points' % read.value
-#    plt.plot(data)
-#except daq.DAQError as err:
-#    print 'DAQmx Error: %s' % err
-#finally:
-#    if task_handle:
-#        # DAQmx Stop Code
-#        daq.DAQmxStopTask(task_handle)
-#        daq.DAQmxClearTask(task_handle)
+TEMPALARM = daq.DAQ(0)
+SYSTEMALARM = daq.DAQ(1)
+data_loop(TEMPALARM, SYSTEMALARM, 100)

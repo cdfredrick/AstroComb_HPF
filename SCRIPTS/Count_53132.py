3ecc44c6357266a8281ce9b51baa1a85402ec79c
Connor Fredrick <connor.fredrick@colorado.edu>
2017-12-17 16:19:28 -0700

    Some GUI stuff and Frequency Counter Template
    Mostly changed the automation of the layout for the GUI. Updated the frequency counter script to reduce dead time.

--- SCRIPTS/Count_53132.py
+++ SCRIPTS/Count_53132.py
@@ -1,3 +1,4 @@
+"""
 # -*- coding: utf-8 -*-
 Created on Mon Jan 20 10:30:45 2014
 
@@ -6,21 +7,16 @@ Created on Mon Jan 20 10:30:45 2014
 # %% Initialize ===============================================================
 
 import visa
-from mongologger import MongoLogger
 from datetime import datetime
-
-ML = MongoLogger()
-
 
 # %% Constants ================================================================
 
-gate_time = 10. # Gate time in seconds
-tm_out_mult = 2. # Multiple of gate time before timeout
-
-
-# %% Load Counter =============================================================
+gate_time = 1. # Gate time in seconds
+tm_out_mult = 2. # Multiple of the gate time before visa timeout
 
 device_name = "GPIB0::16::INSTR"
+
+# %% Load Counter =============================================================
 
 rm = visa.ResourceManager()
 
@@ -30,7 +26,7 @@ if device_name not in rm.list_resources():
 
 # %% Initialize Counter to Known State ========================================
 
-ctr = rm.get_instrument(device_name)
+ctr = rm.open_resource(device_name)
 ctr.clear() # Clear the Counter and Interface
 ctr.write('*RST') # Reset the instrument
 ctr.write('*CLS') # Clear Statue Register and Error Que
@@ -40,63 +36,42 @@ ctr.write(':STAT:PRES') # Preset for operation
 ctr.write(':INP:IMP 50') # Set 50 ohm input impedence (as opposed to 1 mega ohm)
 ctr.write(':ROSC:SOUR EXT') # Set oscillator to external source
 ctr.write(':ROSC:EXT:CHECK OFF') # Don't check for external source
-print ctr.query("*IDN?")
-print datetime.now().strftime('%c')+' -  Initialized'
+ctr.write(':FORMat:DATA ASC')
+print(ctr.query("*IDN?"))
+print(datetime.now().strftime('%c')+' -  Initialized')
 
-ctr.timeout = tm_out_mult*gate_time*1e3
+ctr.timeout = tm_out_mult*gate_time*1e3 # in ms
 
 
 # %% Gating ===================================================================
 
 ctr.write(":FUNC 'FREQ 1'") # Frequency measurement on channel 1
-ctr.write("FREQ:ARM:STAR:SOUR IMM") # Start measuring upon INIT
-ctr.write("FREQ:ARM:STOP:TIM "+str(gate_time)) # Stop after gate time
-# Extend first gate time, to calculate expected freq.
-
-
-# %% Detect Measuring and Service Request (SRQ) ===============================
-
-ctr.write(':STAT:OPER:PTR 0;NTR 16') # Detect transition from measuring to non measuring
-ctr.write(':STAT:OPER:ENABLE 16') # Detect measuring
-ctr.write('*SRE 128') # Assert SRQ on Operation Summary bit
-
+ctr.write(":FREQ:ARM:STAR:SOUR IMM") # Start measuring upon INIT
+ctr.write(":FREQ:ARM:STOP:TIM "+str(gate_time)) # Stop after gate time
 
 # %% Measure f Expected =======================================================
 
-ctr.write('INIT') # Start Measurement
-ctr.wait_for_srq(timeout=(tm_out_mult*gate_time*1e3)) # wait for SRQ
-ctr.write('*CLS') # Clear SRQ
-
-f_exp = ctr.query("FETC:FREQ?") # Read frequency
+f_exp = ctr.query(":READ:FREQ?") # Read frequency
 try:
     float(f_exp)
-    print datetime.now().strftime('%c')+' - Setting Expected Frequency To: '+f_exp
+    print(datetime.now().strftime('%c')+' - Setting Expected Frequency To: '+f_exp)
     ctr.write(':FREQ:EXP: '+f_exp) # Set expected frequency
 except:
-    print datetime.now().strftime('%c')+' - GPIB Read Error: '+f_exp
-    float(f_exp)
-
+    print(datetime.now().strftime('%c')+' - GPIB Read Error: '+f_exp)
 
 # %% Continuous Measurements ==================================================
 
-ctr.write('*CLS') # Clear SRQ
-ctr.write("FREQ:ARM:STOP:TIM "+str(gate_time)) # Set Gate Time
-ctr.write('INIT') # Start Measurement
+# Start Continuous Measurements
+ctr.write(':INIT:CONT')
 
 while True:
-    # Read Measurement --------------------------------------------------------
-    ctr.wait_for_srq(timeout=tm_out_mult*gate_time*1e3) # Wait for SRQ
-    f = ctr.query("FETC:FREQ?") # Read frequency
-
-    # Start Next Measurement --------------------------------------------------
-    ctr.write('*CLS') # Clear SRQ
-    ctr.write('INIT') # Start Measurement
-    
-    # Analysis ----------------------------------------------------------------
+# Read Measurement ------------------------------------------------------------
+    f = ctr.query(":FETC:FREQ?") # Read frequency
+# Analysis --------------------------------------------------------------------
     try:
         float(f)
-        print datetime.now().strftime('%c')+' - Clarity_PFR: '+f
-        ML.logData(f, path="CountFR/clarity")
+        print(datetime.now().strftime('%c')+' - Frequency: '+f)
     except ValueError:
-        print datetime.now().strftime('%c')+' - GPIB Read Error: '+f
+        print(datetime.now().strftime('%c')+' - GPIB Read Error: '+f)
+
 
